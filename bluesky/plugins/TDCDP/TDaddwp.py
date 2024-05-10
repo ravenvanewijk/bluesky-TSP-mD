e758daf4ac649c0f2e97dede22aa282e2ba9f0f2
ravenvanewijk <ravenvanewijk1@gmail.com>
2024-05-10 10:14:02 +0200

    delayed adding of operation at point k (i,j,k tour) + issues file

--- bluesky/plugins/TDCDP/TDaddwp.py
+++ bluesky/plugins/TDCDP/TDaddwp.py
@@ -2,14 +2,14 @@
 import bluesky as bs
 import numpy as np
 import random
-
+from math import isclose
 from bluesky.tools.geo import kwikdist_matrix, qdrdist
 from bluesky.tools.misc import txt2tim
 from bluesky.tools.aero import kts, ft
 from bluesky.core import Entity, timed_function
 from bluesky import stack
 from bluesky.traffic import Route
-from bluesky.plugins.TDCDP.TDdronemanager import DroneManager
+from bluesky.plugins.TDCDP.TDdronemanager import DroneManager, get_wpname
  
 def init_plugin():
     # Configuration parameters
@@ -24,7 +24,6 @@ class Operations(Entity):
     def __init__(self):
         super().__init__()
         self.operational_states = {}
-        self.operation_durations = {}
         self.drone_manager = DroneManager()
     
     @stack.command
@@ -108,15 +107,25 @@ class Operations(Entity):
 
         if wptype.upper() == 'SORTIE':
             if len(args[3].split('/')) == 2:
-                # Check whether coordinates are given. If so, look up wpname
+                # Check whether coordinates are given. If so, look up wpname. If not, None is returned
                 wpname_k = get_wpname(args[3], acrte)
-                wpid_k = acrte.wpname.index(wpname_k)
+                lat_k = args[3].split('/')[0]
+                lon_k = args[3].split('/')[1]
             else:
                 wpname_k = args[3]
-                wpid_k = acrte.wpname.index(args[3].upper())
-            # truck, type, lat_i, lon_i, lat_j, lon_j, lat_k, lon_k, wpname_k, alt, spd
-            child = self.drone_manager.add_drone(acid, args[0], acrte.wplat[wpid], acrte.wplon[wpid], 
-                args[1], args[2], acrte.wplat[wpid_k], acrte.wplon[wpid_k], wpname_k, args[4], args[5])
+            try:
+                wpid_k = acrte.wpname.index(wpname_k.upper())
+                # truck, type, lat_i, lon_i, lat_j, lon_j, lat_k, lon_k, wpname_k, alt, spd
+                child = self.drone_manager.add_drone(acid, args[0], acrte.wplat[wpid], acrte.wplon[wpid], 
+                    args[1], args[2], acrte.wplat[wpid_k], acrte.wplon[wpid_k], wpname_k, args[4], args[5])
+            except AttributeError:
+                # This except block is used when wpname_k is None. The coordinates will be stored with the /
+                # This means the operation point cannot be added yet since it is not a part of the truck route yet
+                # Dronemanager class will recognize this and delay adding the operational point
+                wpid_k = wpname_k
+                child = self.drone_manager.add_drone(acid, args[0], acrte.wplat[wpid], acrte.wplon[wpid], 
+                    args[1], args[2], None, None, args[3], args[4], args[5])
+
             if acrte.children[wpid][0] is None:
                 acrte.children[wpid] = [child]
             else:
@@ -142,8 +151,11 @@ class Operations(Entity):
             acidx = bs.traf.id.index(acid)
             acrte = Route._routes[acid]
             iactwp = acrte.iactwp
+            # if len(acrte.wpname) > iactwp+2:
+            #     if acrte.wpname[iactwp] == 'TRUCK469' or acrte.wpname[iactwp] == 'TRUCK468' or acrte.wpname[iactwp] == 'TRUCK470' or acrte.wpname[iactwp] == 'TRUCK471' or acrte.wpname[iactwp] == 'TRUCK467':
+            #         print(iactwp, acrte.wpname[iactwp])
             if iactwp == -1:
-                # Manually set active wp to 0 if AC is at start, ensure that WP1 can also be an operation
+                # Manually set active wp to 0 if vehicle is at start, ensure that WP1 can also be an operation
                 iactwp = 0
             # check whether or not the attribute exists. Will not exist if regular addwaypoints is called
             if hasattr(acrte, 'operation_wp'):
@@ -230,18 +242,3 @@ class Operations(Entity):
         stack.stack(f'ATSPD {acid} 5 VNAV {acid} ON')
         # delivery is done, remove from the delivery states dict
         self.operational_states.pop(acid, None)
-
-def get_wpname(wpname, acrte, acc=11):
-    """Gets the wpname of a wp by looking for its coords.
-    args: type, description
-    wpname: string, coordinates lat lon separated by /, wp to look up
-    acrte: route object, route of the AC"""
-    lat, lon = wpname.split('/')
-    lat = str(round(float(lat), 11))
-    lon = str(round(float(lon), acc))
-    wps = list(zip([ f'%.{acc}f' % elem for elem in acrte.wplat],[ f'%.{11}f' % elem for elem in acrte.wplon]))
-    try:
-        match_index = next(i for i, pair in enumerate(wps) if pair == (lat, lon))
-        return acrte.wpname[match_index]
-    except StopIteration:
-        raise Exception(f'WP with coordinates {lat}, {lon} not found')
--- bluesky/plugins/TDCDP/TDdronemanager.py
+++ bluesky/plugins/TDCDP/TDdronemanager.py
@@ -3,6 +3,7 @@ from bluesky.core import Entity, timed_function
 from bluesky import stack
 from bluesky.tools.geo import qdrdist
 from bluesky.traffic import Route
+from math import isclose
 
 def init_plugin():
  
@@ -49,12 +50,21 @@ class DroneManager(Entity):
         stack.stack(f"{drone_name} ATALT {data['alt']} SPDAP {drone_name} {data['spd']}")
         scen_text = f"ADDTDWAYPOINTS {drone_name} "
         for wp in ['i', 'j', 'k']:
+            if data['lon_k'] is None and wp == 'k':
+                # This means coordinates are given but embedded in the wpname
+                # Add the waypoint but do not add the drone to the routes to modify, since the truck does not have
+                # this waypoint yet
+                data['lat_k'], data['lon_k'] = data['wpname_k'].split('/')
+            elif wp == 'k':
+                self.routes_to_modify[drone_name] = {'wpname_k': data['wpname_k'], 'truck': data['truck']}
+            
             scen_text += f"{data['lat_' + wp]} {data['lon_' + wp]} {data['alt']} {data['spd']} TURNSPD 5 "
 
         stack.stack(scen_text)
         stack.stack(f"LNAV {drone_name} ON")
         stack.stack(f"VNAV {drone_name} ON")
-        self.routes_to_modify[drone_name] = {'wpname_k': data['wpname_k'], 'truck': data['truck']}
+        # Add operation for the delivery
+        stack.stack(f"ADDOPERATIONPOINTS {drone_name} {data['lat_j']}/{data['lon_j']} DELIVERY 5")
 
     def complete_sortie(self, drone_name):
         if drone_name in self.active_drones:
@@ -67,16 +77,48 @@ class DroneManager(Entity):
     def retrieve_drone(self, drone_name):
         stack.stack(f"DEL {drone_name}")
 
-    @timed_function
+    @timed_function(dt = 10)
     def modify_wp(self):
         to_remove = []
         for drone_name in self.routes_to_modify:
             acrte = Route._routes[drone_name]
-            stack.stack(f"ADDOPERATIONPOINTS {drone_name} {acrte.wpname[-2]} DELIVERY 5")
             stack.stack(f"ADDOPERATIONPOINTS {drone_name} {acrte.wpname[-1]} RENDEZVOUS 1")
             stack.stack(f"ADDOPERATIONPOINTS {self.routes_to_modify[drone_name]['truck']} \
-                        {self.routes_to_modify[drone_name]['wpname_k']} RENDEZVOUS 1 {drone_name}")
+                    {self.routes_to_modify[drone_name]['wpname_k']} RENDEZVOUS 1 {drone_name}")
             to_remove.append(drone_name)
         
         for key in to_remove:
             self.routes_to_modify.pop(key)
+
+    @timed_function(dt = 10)
+    def add_rendezvous(self):
+        for drone_name in self.active_drones:
+            if self.active_drones[drone_name]['status'] == 'OTW' and \
+                    len(self.active_drones[drone_name]['wpname_k'].split('/')) == 2:
+                truckrte = Route._routes[self.active_drones[drone_name]['truck']]
+                wpname_k = get_wpname(self.active_drones[drone_name]['wpname_k'], truckrte, acc=11)
+                if wpname_k is None:
+                    continue
+                print(f'Adding operation point k for {drone_name}')
+                wpid_k = truckrte.wpname.index(wpname_k.upper())
+                self.active_drones[drone_name]['lat_k'] = truckrte.wplat[wpid_k]
+                self.active_drones[drone_name]['lon_k'] = truckrte.wplon[wpid_k]
+                self.active_drones[drone_name]['wpname_k'] = wpname_k
+                data = self.active_drones[drone_name]
+                # Adding the drone to this dictionary ensures that its waypoint (and the truck) at k will be operations
+                self.routes_to_modify[drone_name] = {'wpname_k': data['wpname_k'], 'truck': data['truck']}
+
+def get_wpname(wpname, acrte, acc=6, tol=1e-6):
+    """Gets the wpname of a wp by looking for its coords.
+    args: type, description
+    wpname: string, coordinates lat lon separated by /, wp to look up
+    acrte: route object, route of the AC
+    tol: float, tolerance for coordinate matching
+    """
+    lat, lon = map(float, wpname.split('/'))
+    lat = round(lat, acc)
+    lon = round(lon, acc)
+    
+    for i, (plat, plon) in enumerate(zip(acrte.wplat, acrte.wplon)):
+        if isclose(plat, lat, abs_tol=tol) and isclose(plon, lon, abs_tol=tol):
+            return acrte.wpname[i]
