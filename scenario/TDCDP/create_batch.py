4a5aac0a1cb9fded404b7c5d88164317ad445df5
ravenvanewijk <ravenvanewijk1@gmail.com>
2024-09-19 10:35:11 +0200

    fixed batch file

--- scenario/TDCDP/create_batch.py
+++ scenario/TDCDP/create_batch.py
@@ -1,7 +1,8 @@
 import os
 
 folders = [x for x in os.listdir('scenario/TDCDP') if '.py' not in x \
-                                                    and '.scn' not in x]
+                                                    and '.scn' not in x\
+                                                    and '.DS' not in x]
 
 batchscn = open("scenario/TDCDP/batchtdcdp.scn", "w")
 
@@ -11,5 +12,5 @@ for folder in folders:
         if 'Seattle' in file:
             continue
         batchscn.write(f'00:00:00.00>SCEN {folder}_{file}\n')
-        batchscn.write(f'00:00:00.00>PCALL {folder}/{file}\n')
+        batchscn.write(f'00:00:00.00>PCALL TDCDP/{folder}/{file}\n')
         batchscn.write('00:00:00.00>FF\n\n')
